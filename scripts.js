01fbe60d36a26ded9e14e216e87a52be1f61303f
skelyan <153768209+skelyan@users.noreply.github.com>
2024-12-16 09:25:06 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 function nextEscapeRoomPage() {
-  window.location.href = "https://44rmzx.csb.app/";
+  window.location.href = "https://www.canva.com/design/DAGY7VWxnS4/0UxUK7FoEK249m6xIltO4A/edit";
 }
